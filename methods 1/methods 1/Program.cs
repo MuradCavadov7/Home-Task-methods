06d075c61d818748ba4b8ddc6ba6a90969eb459f
MuradCavadov7 <muradnc-ap117@code.edu.az>
2024-10-12 21:34:51 +0400

    overload tasks and Default value

---
+++ methods 1/methods 1/Program.cs
@@ -0,0 +1,62 @@
+﻿
+namespace methods_1
+{//Overload Tasks 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1. Çevrənin sahəsi - S = p*r² (p=3)
+            int radius = 10;
+            int circleArea = Area(radius);
+            Console.WriteLine(circleArea);
+            #endregion
+
+            #region 2. Düzbucaqlının sahəsi - S = a*b
+            //int eni = 5;
+            //int uzunlugu = 10;
+            //int rectangularArea=Area(eni, uzunlugu);
+            //Console.WriteLine(rectangularArea);
+            #endregion
+
+            #region 3. Düzbucaqlı paralelpipedin tam səthinin sahəsi -S = 2(a * b + a * c + b * c)
+            //int aTerefi = 3;
+            //int bTerefi=4;
+            //int cTili = 5;
+            //int duzbucaqliParalipedArea=Area(aTerefi, bTerefi, cTili);
+            //Console.WriteLine(duzbucaqliParalipedArea);
+            #endregion
+
+
+            #region 4. Üçbucaqlının daxilinə çəkilmiş çevrənin sahəsi - S=p*r; p=(a+b+c)/2
+            //int a = 5;
+            //int b = 3;
+            //int c = 4;
+            //int radius = 10;
+            //int daxileCevreArea=Area(a, b, c,radius);
+            //Console.WriteLine(daxileCevreArea);
+
+            #endregion
+
+        }
+        static int Area(int radius)
+        {
+            int pi = 3;
+            
+            return pi * radius * radius;
+        
+        }
+        static int Area(int eni, int uzunlugu)
+        {
+            return (eni * uzunlugu);
+        }
+        static int Area(int aTerefi,  int bTerefi, int cTili)
+        {
+            return 2 * (aTerefi * bTerefi + aTerefi* cTili + bTerefi * cTili);
+        }
+        static int Area(int a,int b,int c,int radius)
+        {
+            int p =(a+b+c)/2;
+            return p*radius;
+        }
+    }
+}
---
+++ methods2/methods2/Program.cs
@@ -0,0 +1,75 @@
+﻿
+namespace methods2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region c.Methoda ad gonderende(Sabir),a ve s gonderende(Guliyev Sabir),a,s ve ata a göndərildikdə adin(S.Guliyev Mehti).
+            //string name = Console.ReadLine();
+            //string surname= Console.ReadLine();
+            //string fatherName = Console.ReadLine();
+            //Biography(name);
+            #endregion
+            #region b.əgər methoda bir eded gonderilirse hemin ededin kvadratını,iki eded gonderilirse 1ci ədədi ikinci ədəd qədər qüvvətinə yüksəldirsiz.
+            //Console.WriteLine(POW(2));
+            #endregion
+
+            #region a.Method yaradirsiz 2 eded ve 1 operator('+','-','*','/') qebul edir. Qebul etdiyi operatora uygun a v' b ədədləri üzərində hesablama aparib alinan deyeri qaytarır.
+            //Console.WriteLine(Aricmetic(3,5,(char)43));
+            #endregion
+
+        }
+        static void Biography(string name)//C misali
+        {
+            Console.WriteLine(name);
+        }
+       static void Biography(string name,string surname)
+        {
+            Console.WriteLine($"{surname}{" "}{name}");
+        }
+
+        static void Biography(string name,string surname,string fatherName)
+        {
+            char basHerf = name[0];
+            Console.WriteLine($"{basHerf+"."}{surname}{" "}{fatherName}");
+        }
+
+        static int POW(int num1, int num2=2)//B misali
+        { int quvvet = 1;
+            for (int i = 0; i <num2 ; i++)
+            {
+               quvvet *= num1;
+                
+            }
+            return quvvet;
+
+        }
+        
+        static int Aricmetic(int number1,int number2,char operators)//A misali
+        {
+            if (operators == (char)42)
+            {
+                return number1 * number2;
+            }
+            else if (operators == (char)43)
+            {
+                return number1 + number2;
+            }
+            else if (operators == (char)45)
+            {
+                return number1 - number2;
+            }
+            else if (operators == (char)47)
+            {
+                return number1 / number2;
+            }
+            else 
+            {
+                return 0;
+            }
+        }
+
+
+    }
+}
