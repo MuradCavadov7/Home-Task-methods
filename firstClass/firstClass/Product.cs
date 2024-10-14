c8cede61e5aec2b9a91b2d5d60543db356dfbe0a
MuradCavadov7 <muradnc-ap117@code.edu.az>
2024-10-14 22:43:04 +0400

    product class

---
+++ firstClass/firstClass/Product.cs
@@ -0,0 +1,19 @@
+﻿
+namespace firstClass
+{
+    class Product
+    {
+        public string Name;
+        public string BrandName;
+        public double Price;
+
+        public Product(string name, string brandname, double Price)
+        {
+            this.Name = name;
+            this.BrandName = brandname;
+            this.Price = Price;
+
+        }
+       
+    }
+}
---
+++ firstClass/firstClass/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace firstClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] product = new Product[]
+            {
+                new Product("Clothes", "Colin's", 235.65),
+                 new Product("Trousers", "Dolce&Gabbana", 340.45),
+                 new Product("Shoes", "NIKE", 200.25),
+                 new Product("Jacket","Burberry",1935.20),
+                 new Product("Coat","FURSAC",739.39),
+                 new Product("Watch","HUBLOD",15600.75)
+
+            };
+            magazineSystem(product, 300.50, 5000.100);
+        }
+
+         static void magazineSystem( Product[] product, double minPrice, double maxPrice)
+        {
+            Console.WriteLine($"Mehsul {minPrice} - {maxPrice} arasindadir \n ");
+            foreach (var products in product) 
+            {
+                if (products.Price>=minPrice && products.Price <= maxPrice)
+                {
+                    Console.WriteLine($"Mehsul:{products.Name} Qiymeti:{products.Price}$");
+                }
+                else
+                {
+                    
+                    Console.WriteLine("Mehsul uygun deyil");
+                }
+            }
+
+        }
+    }
+}
