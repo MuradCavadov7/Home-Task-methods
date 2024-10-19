9dd8a16a6e4852d4ad7153554ffed52b6eb71f13
MuradCavadov7 <muradnc-ap117@code.edu.az>
2024-10-19 14:32:23 +0400

    Singer and Song task

---
+++ Playlist/Playlist/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Playlist;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Singer paster = new Singer("Paster", "Blessed", 35);
+        Song song = new Song("Zengezur", "Rap", paster);
+        song.AddRating(9.3f);
+        song.AddRating(8.4f);
+        song.AddRating(9.5f);
+        song.GetAverageRating();
+    }
+}
+
---
+++ Playlist/Playlist/Song.cs
@@ -0,0 +1,99 @@
+﻿using System.ComponentModel.Design;
+
+namespace Playlist;
+
+internal class Song
+{
+    private string _name;
+
+    public string Name
+    {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            if (value.Length <= 100)
+                _name = value;
+        }
+    }
+
+    private string _genre;
+
+    public string Genre
+    {
+        get
+        {
+            return _genre;
+        }
+        set
+        {
+            if (value == "Pop" || value == "Rock" || value == "Jazz" || value == "Rap" || value == "Techno")
+            {
+                _genre = value;
+            }
+            else
+            {
+                Console.WriteLine("zehmet olmasa 'Pop','Rock','Jazz','Rap','Techno' Janrlarina uz tutun");
+            }
+        }
+    }
+    private Singer _aggregation;
+
+    public Singer Aggregation
+    {
+        get
+        {
+            return _aggregation;
+        }
+        set
+        {
+            _aggregation = value;
+        }
+    }
+    private double _totalRating = 0;
+    private int _ratingCount = 0;
+
+
+
+    public Song(string name, string genre, Singer aggragation)
+    {
+        Name = name;
+        Genre = genre;
+        Aggregation = aggragation;
+    }
+
+    public void AddRating(float rating)
+    {
+        if (Genre == "Pop" || Genre == "Rock" || Genre == "Jazz" || Genre == "Rap" || Genre == "Techno")
+        {
+            if (rating >= 0 && rating <= 10)
+            {
+                _ratingCount++;
+                _totalRating += rating;
+            }
+            else
+            {
+                Console.WriteLine("Zehmet olmasa 0-10 arasinda rating daxil edin");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Janr duzgun secilmediyi ucun rating elave oluna bilmir");
+        }
+    }
+    public void GetAverageRating()
+    {
+        if (Genre == "Pop" || Genre == "Rock" || Genre == "Jazz" || Genre == "Rap" || Genre == "Techno")
+        {
+            double averageRating = 0;
+            averageRating = _totalRating / _ratingCount;
+            Console.WriteLine($"Rating status {averageRating} from {_ratingCount} Person");
+        }
+        else
+        {
+            Console.WriteLine("Janr duzgun secilmediyi ucun rating ortalamasi hesablana bilmir");
+        }
+    }
+}
