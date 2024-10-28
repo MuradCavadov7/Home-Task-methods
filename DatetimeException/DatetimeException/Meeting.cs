c8302be093ae8fb48f064b4d2815d4ebdd9b14ac
MuradCavadov7 <muradnc-ap117@code.edu.az>
2024-10-29 00:09:33 +0400

    datetime exception

---
+++ DatetimeException/DatetimeException/Meeting.cs
@@ -0,0 +1,56 @@
+﻿namespace DatetimeException;
+
+public class Meeting
+{
+    public DateTime FromDate { get; set; }
+    public DateTime ToDate { get; set; }
+    public string FullName { get; set; }
+    public Meeting(DateTime fromDate, DateTime toDate, string fullName)
+    {
+        FromDate = fromDate;
+        ToDate = toDate;
+        FullName = fullName;
+    }
+}
+public class MeetingSchedule
+{
+    public static Meeting[] Meetings { get; set; }
+    int meetingCount = 0;
+    public MeetingSchedule(int meeting)
+    {
+        Meetings = new Meeting[meeting];
+    }
+    public void SetMeeting(string fullname, DateTime from, DateTime to)
+    {
+        if (from >= to)
+        {
+            throw new WrongDateInterval($"{from} tarixi {to} dan boyuk ola bilmez");
+        }
+
+        for (int i = 0; i < meetingCount; i++)
+        {
+            if (from < Meetings[i].ToDate && to > Meetings[i].FromDate)
+            {
+                throw new ReservedDateInterval("Gonderilen tarix intervalda movcuddur");
+            }
+
+        }
+        if (meetingCount < Meetings.Length)
+        {
+            Meetings[meetingCount++] = new Meeting(from, to, fullname);
+            Console.WriteLine($"Yeni gorus{fullname} {from} {to} elave olundu");
+        }
+
+    }
+
+}
+
+
+public class ReservedDateInterval : Exception
+{
+    public ReservedDateInterval(string message) : base(message) { }
+}
+public class WrongDateInterval : Exception
+{
+    public WrongDateInterval(string message) : base(message) { }
+}
---
+++ DatetimeException/DatetimeException/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DatetimeException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MeetingSchedule meetingSchedule = new MeetingSchedule(3);
+            try
+            {
+                meetingSchedule.SetMeeting("Cavadov Murad", new DateTime(2024, 11, 1, 9, 0, 0), new DateTime(2024, 11, 1, 10, 0, 0));
+                meetingSchedule.SetMeeting("Cavadov Yunis", new DateTime(2024, 11, 1, 10, 30, 0), new DateTime(2024, 11, 1, 11, 30, 0));
+                meetingSchedule.SetMeeting("Cavadov Murad", new DateTime(2024, 11, 1, 10, 0, 0), new DateTime(2024, 11, 1, 11, 0, 0));
+            }
+            catch (ReservedDateInterval ex)
+            {
+                Console.WriteLine($"Meeting conflict: {ex.Message}");
+            }
+            catch (WrongDateInterval ex)
+            {
+                Console.WriteLine($"Meeting conflict: {ex.Message}");
+            }
+
+            try
+            {
+
+                meetingSchedule.SetMeeting("Cavadov Elmar", new DateTime(2024, 11, 2, 14, 0, 0), new DateTime(2024, 11, 2, 13, 0, 0));
+            }
+            catch (ReservedDateInterval ex)
+            {
+                Console.WriteLine($"Meeting conflict: {ex.Message}");
+            }
+            catch (WrongDateInterval ex)
+            {
+                Console.WriteLine($"Invalid date interval: {ex.Message}");
+            }
+        }
+    }
+}
